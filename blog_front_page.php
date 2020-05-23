b0bc3db42383b631ad096a909086cda116a386cf
Suganthi-K <64631025+Suganthi-K@users.noreply.github.com>
2020-05-23 22:29:12 +0530

    Add files via upload

---
+++ blog_front_page.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<title> Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+
+</style><?php 
+include "../translate.php";
+?>
+<body style="background-color:#333333"id="myPage">
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:960px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1 style="color:#FFFFFF"><b> BLOG</b></h1>
+  <p style="color:#FFFFFF">Welcome to the blog of <span class="w3-tag">Madurai COVID-19 Tracker</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col 18 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="pic/28.png"  style="width:100%">
+    <div class="w3-container">
+      <h3><b >Symptoms of Corono Virus Disease</b></h3>
+      <h5>Publication Date, <span class="w3-opacity">May 23, 2020</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don't develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment..................      </p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b><a href="blog1/blog_1.php">READ MORE »</a></b></button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="pic/29.png" style="width:100%">
+    <div class="w3-container ">
+      <h3><b >What is the recovery time for the coronavirus disease?</b></h3>
+      <h5>Publication Date, <span class="w3-opacity">May 23, 2020</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>Using available preliminary data, the median time from onset to clinical recovery for mild cases is approximately 2 weeks and is 3-6 weeks for patients with severe or critical disease........</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b><a href="blog1/blog_2.php">READ MORE »</a></b></button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="pic/mask.png" style="width:100%">
+    <div class="w3-container "><h3><b>Are masks effective against the coronavirus disease?</b></h3>
+      <h5>Publication Date, <span class="w3-opacity">May 23, 2020</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p> If you are healthy, you only need to wear a mask if you are taking care of a person with suspected 2019-nCoV infection. Wear a mask if you are coughing or sneezing. Masks are effective only when used in combination with frequent hand-cleaning with alcohol-based hand rub or soap and water. If you wear a mask, then you must know how to use it and dispose of it properly........</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b><a href="blog1/blog_3.php">READ MORE »</a></b></button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+ <div class="container " style="font-size:18px" style="color:#000000">
+<ul class="pager font-weight-bold text-monospace text-capitalize text-danger">
+  <li class="center"><a href="../index.php">Back to home page</a></li>
+</ul></div>
+<!-- Footer -->
+<footer class="w3-center w3-light-grey w3-padding-20 w3-large container-fluid text-center" >  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Copyright@ <a href="https://www.credihealth.com" title="credihealth.com" target="_blank" class="w3-hover-text-green">credihealth.com</a><br> <a href="https://www.reuters.com/article/us-health-mood-heart/anxiety-depression-tied-to-higher-risk-of-heart-attack-stroke-idUSKCN1LD2GN"title="reuters.com" target="_blank" class="w3-hover-text-green">  reuters.com</a></p>
+</footer>
+
+</body>
+</html>
